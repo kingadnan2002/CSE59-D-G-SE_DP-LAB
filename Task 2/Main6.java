6dbf693249922f59739136c084f2ce5d6743a429
kingadnan2002 <wahdiadnan14@gmail.com>
2026-06-18 16:35:46 +0600

    Update Main6.java

--- Task 2/Main6.java
+++ Task 2/Main6.java
@@ -1,1 +1,19 @@
+class Applet {
+    void launch() {
+        System.out.println("Applet launched");
+    }
+}
 
+class GameApplet extends Applet {
+    void play() {
+        System.out.println("Playing game");
+    }
+}
+
+public class Main1 {
+    public static void main(String[] args) {
+        GameApplet myGame = new GameApplet();
+        myGame.launch();
+        myGame.play();
+    }
+}
