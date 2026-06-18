bcd9b0aa8a44da914eb4ec013c68aa30d401d8b9
kingadnan2002 <wahdiadnan14@gmail.com>
2026-06-18 16:34:11 +0600

    Update Main5.java

--- Task 2/Main5.java
+++ Task 2/Main5.java
@@ -1,1 +1,19 @@
+class Printer {
+    void printData(String text) {
+        System.out.println("Printing text: " + text);
+    }
 
+    void printData(int number) {
+        System.out.println("Printing number: " + number);
+    }
+}  
+
+
+public class Main1 {
+	public static void main(String[] args) {
+		Printer printer = new Printer();
+		
+		printer.printData("Hello World");
+		printer.printData(42);
+	}
+}
