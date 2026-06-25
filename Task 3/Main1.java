ee3654ddf6ba4eaa0dc80eade6c3f374d8286fc7
kingadnan2002 <wahdiadnan14@gmail.com>
2026-06-25 16:58:07 +0600

    Update Main1.java

--- Task 3/Main1.java
+++ Task 3/Main1.java
@@ -1,1 +1,59 @@
+public class main1 {
 
+	    public static void main(String[] args) {
+
+	        User user = new User("john_doe", "john@example.com");
+
+	        EmailValidator validator = new EmailValidator();
+	        UserRepository repository = new UserRepository();
+
+	        if (validator.isValid(user.getEmail())) {
+	            repository.save(user);
+	        }
+	    }
+	}
+
+	class User {
+	    private String username;
+	    private String email;
+
+	    public User(String username, String email) {
+	        this.username = username;
+	        this.email = email;
+	    }
+
+	    public String getUsername() {
+	        return username;
+	    }
+
+	   public String getEmail() {
+		   return email;
+		   
+	     }
+	   
+	   }
+
+	 class EmailValidator {
+		 
+		 public boolean isValid(String email) {
+			 if(email !=null && email.contains("@")) {
+				 return true;
+				 
+			 }
+			 
+			 System.out.println("Invakid email format.");
+			 return false;
+		 }
+	 }
+  
+	 
+	    class UserRepository {
+	    	
+	    	public void save(User user) {
+	    		System.out.println("Connecting to database...");
+	    		
+	    		System.out.println("Saving user" + user.getUsername()+ "to the users table.");
+	    	}
+	    }
+	 
+	 
